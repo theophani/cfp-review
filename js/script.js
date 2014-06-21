8f677315d7d81377b961d819e76cd39abbb707a9
Tiffany Conroy <tiffany.conroy@gmail.com>
2014-06-21 17:27:29 +0200

    Move Jan's work into the external script

--- js/script.js
+++ js/script.js
@@ -1,45 +1,73 @@
 (function () {
 
+  // localStorage warppers
+  Storage.prototype.setObject = function(key, value) {
+    this.setItem(key, JSON.stringify(value));
+  };
+
+  Storage.prototype.getObject = function(key) {
+    try {
+      return JSON.parse(this.getItem(key));
+    } catch(e) {
+      return {};
+    }
+  };
+  // ---
+
+  // loading/storing votes
+  var ls_key = 'jseu-voting';
+  function loadVotes() {
+    if(!window.localStorage.getObject(ls_key)) {
+      window.localStorage.setObject(ls_key, []);
+    }
+    return window.localStorage.getObject(ls_key);
+  }
+
+  function storeVotes(votes) {
+    window.localStorage.setObject(ls_key, votes);
+  }
+  // ---
+
   var sheetID = "ENTER THE SHEET ID HERE";
   var spreadsheetLink = "https://docs.google.com/a/github.com/spreadsheet/ccc?key=" + sheetID;
-  var cellUrlPrefix = "https://spreadsheets.google.com/feeds/cells/"+ sheetID +"/ocw/public/basic/";
 
-  var voteColumn = 25;  // needs to be based on who is voting
-  var remarkColumn = 26;  // needs to be based on who is voting
-
-  function cellAddress(row, column) {
-    return 'R' + row + 'C' + column;
-  }
-
-  function createEntryElem(row, column, inputValue) {
-    var entry = document.createElement('entry');
-    entry.setAttribute('xmlns', "http://www.w3.org/2005/Atom");
-    entry.setAttribute('xmlns:gs', "http://schemas.google.com/spreadsheets/2006");
-    entry.innerHTML = '<id>https://spreadsheets.google.com/feeds/cells/key/'+sheetID+'/private/full/'+cellAddress(row, column)+'</id><link rel="edit" type="application/atom+xml" href="https://spreadsheets.google.com/feeds/cells/key/'+sheetID+'/private/full/'+cellAddress+'"/><gs:cell row="'+row+'" col="'+column+'" inputValue="'+inputValue+'"/>';
-    return entry;
-  }
-
-  function persistChoice (column) {
-    return function (e) {
-      $('body').css({ opacity: 0.5 });
-      var row = $('[name=sheetRowNumber]').val();
-      var choice = e.target.value;
-      var entry = createEntryElem(row, column, choice);
-
-      $.ajax({
-        crossDomain: true,
-        type: 'PUT',
-        url: cellUrlPrefix + cellAddress(row, column),
-        contentType: 'application/atom+xml',
-        data: entry,
-        success: function () {
-          $('body').css({ opacity: 1 });
-        }
-      });
+  // restore previous votes into the current vote form
+  function loadValues() {
+    var votes = loadVotes();
+    var rowNo = parseInt($('#sheetRowNumber').val(), 10);
+    // load persisted values
+    var vote = votes[rowNo];
+    if(!vote) {
+      return;
     }
+    $('#comment').val(vote.comment);
+    $('#vote_' + vote.vote).prop("checked", true);
   }
 
+  function persistChoice (e) {
+    var vote = parseVote(($('#voter').serialize()));
+    var votes = loadVotes();
+
+    votes[vote.sheetRowNumber] = {
+      vote: vote.vote,
+      comment: vote.comment
+    };
+    storeVotes(votes);
+    // need to know who is voting i.e. which columns to persist vote and remarks
+    function successCallback () {
+      $('body').css({ opacity: 1 });
+    }
+    $('body').css({ opacity: 0.5 });
+    setTimeout(successCallback, 150);
+  }
+
+  function persistRemark (e) {
+    persistChoice(e);
+  }
+
+  var totalRows; // ugh global!
   function showInfo(data) {
+    totalRows = data.length;
     data = data.map(function (proposal) {
       proposal.summary = proposal["summarytobeusedonsiteplaintextormarkdown3-5sentences."];
       proposal.extra = proposal["whatelseyouwanttotellusaboutthetalkoutsidethepublicsummary3-5sentences"];
@@ -50,6 +78,7 @@
     , "pagination": 1, "tableDiv": "#fullTable", "filterDiv": "#fullTableFilter"}
     Sheetsee.makeTable(tableOptions)
     Sheetsee.initiateTableFilter(tableOptions)
+    loadValues();
     $('body').removeClass('loading');
   }
 
@@ -57,6 +86,40 @@
     Tabletop.init( { key: sheetID, callback: showInfo, simpleSheet: true } )
   })
 
-  $('.content').on('change', 'form input', persistChoice(voteColumn));
-  $('.content').on('blur', 'form textarea', persistChoice(remarkColumn));
+  // localStorage to csv
+  function exportData(e) {
+    e.preventDefault();
+    var votes = loadVotes();
+    var csv = '';
+    for(var idx = 0; idx < totalRows; idx++) {
+      var vote = votes[idx];
+      if(vote) {
+        csv += vote.vote + ',' + vote.comment + '\n';
+      } else {
+        csv += '\n';
+      }
+    }
+    $('#export').val(csv).show();
+  }
+
+  // helper
+  function parseVote(s) {
+    var vote = {};
+    var kvs = s.split('&');
+    kvs.forEach(function(kv) {
+      var k_v = kv.split('=');
+      var key = k_v[0];
+      var value = k_v[1];
+      if(key != 'comment') {
+        value = parseInt(value, 10);
+      }
+      vote[k_v[0]] = value;
+    });
+    return vote;
+  }
+
+  $('.content').on('change', 'form input', persistChoice);
+  $('.content').on('blur', 'form textarea', persistRemark)
+  $('#export-link').on('click', exportData);
+  $(window).on('hashchange', loadValues);
 }());
